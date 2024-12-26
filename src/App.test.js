cb54e4c067a6985fc536c5546b4df9e012e5fc63
Fauzul Ashari <p4ujul@gmail.com>
2024-12-26 20:19:46 +0700

    feat!: Started migrating to NextJS v14 + TS

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("Render App", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Manggaleh Shop/i);
-  expect(linkElement).toBeInTheDocument();
-});
