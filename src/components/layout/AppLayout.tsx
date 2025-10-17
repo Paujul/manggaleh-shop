6bcb87c7f79716d8d90a8686ca8f4c9faf10fa0c
Paujul <p4ujul@gmail.com>
2025-10-18 01:09:40 +0700

    feat: add toaster

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import { Toaster } from '../ui/sonner'
 
 const AppLayout = () => (
   <div className='bg-[#F7F7F6] min-h-[100dvh] flex flex-col'>
     <Navbar />
     <Outlet />
     <Footer />
+    <Toaster position='top-right' expand={true} />
   </div>
 )
 
