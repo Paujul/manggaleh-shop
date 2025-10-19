2904c5eb253bfd7a19cf7782f53ab51b377a20e4
Paujul <p4ujul@gmail.com>
2025-10-20 01:33:49 +0700

    feat: add edit product handler

--- src/components/product/dashboard/table/section/ActionPopover.tsx
+++ src/components/product/dashboard/table/section/ActionPopover.tsx
@@ -1,9 +1,31 @@
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
+import type { Product } from '@/types/product'
 import { SquarePen, Trash2 } from 'lucide-react'
+import FormDialog from './FormDialog'
+import { useState } from 'react'
 
-function ActionPopover() {
+type Props = {
+  product: Product
+}
+
+function ActionPopover({ product }: Props) {
+  const [isFormModalOpen, setIsFormModalOpen] = useState<boolean>(false)
   return (
     <div className='flex gap-4'>
-      <SquarePen size={22} />
+      <Dialog open={isFormModalOpen} onOpenChange={setIsFormModalOpen}>
+        <DialogTrigger>
+          <SquarePen size={22} />
+        </DialogTrigger>
+
+        <DialogContent className='px-4 py-5' showCloseButton={false}>
+          <FormDialog
+            open={isFormModalOpen}
+            onOpenChange={setIsFormModalOpen}
+            isEdit={true}
+            product={product}
+          />
+        </DialogContent>
+      </Dialog>
       <Trash2 size={22} color='red' />
     </div>
   )
