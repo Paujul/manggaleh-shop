08c9f7a4920a9e43cf154b8928b0df830ae9806f
Paujul <p4ujul@gmail.com>
2025-10-20 14:33:00 +0700

    Add onClick property

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,16 +1,17 @@
 import { cn } from '@/utils/cn'
 import { ChevronDown } from 'lucide-react'
-import type { ReactNode } from 'react'
+import type { MouseEventHandler, ReactNode } from 'react'
 
 type ButtonProps = {
   children: ReactNode
   className?: string
   dropdown?: boolean
+  onClick?: MouseEventHandler<HTMLButtonElement>
 }
 
-function Button({ children, className, dropdown }: ButtonProps) {
+function Button({ children, className, dropdown, onClick }: ButtonProps) {
   return (
-    <button className={cn('button', className)}>
+    <button className={cn('button', className)} onClick={onClick}>
       {children}
 
       {dropdown && <ChevronDown size={20} color='var(--color-gray-400)' />}
