71fbaad46e39c421e32fd5a516ed94f631337f9a
Paujul <p4ujul@gmail.com>
2025-10-21 19:38:53 +0700

    Change supabase anon key name

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,5 +1,5 @@
 export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
-export const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
+export const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
 export const SUPABASE_ROLE_KEY = import.meta.env.VITE_SUPABASE_ROLE_KEY
 
 export const CLOUDINARY_CLOUD_ID = import.meta.env.VITE_CLOUDINARY_CLOUD_ID
