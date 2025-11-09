53065a28623d2f55c9f8c6f30053a3218cc9d834
Paujul <p4ujul@gmail.com>
2025-11-09 14:54:39 +0700

    Add reusable form input component

---
+++ src/components/form/inputs/FormFieldInput.tsx
@@ -0,0 +1,49 @@
+import type {
+  FieldValues,
+  Path,
+  RegisterOptions,
+  UseFormRegister,
+} from 'react-hook-form'
+
+import { cn } from '@/utils/cn'
+
+type ComponentProps<T extends FieldValues> = {
+  label: Path<T>
+  defaultValue?: string | number
+  register: UseFormRegister<T>
+  inputConfig: RegisterOptions<T>
+  type?: React.HTMLInputTypeAttribute
+  className?: string
+  price?: boolean
+}
+
+function FormFieldInput<T extends FieldValues>({
+  label,
+  defaultValue,
+  register,
+  inputConfig,
+  type = 'text',
+  price,
+}: ComponentProps<T>) {
+  return (
+    <div className='relative flex flex-col gap-1'>
+      <label htmlFor={label} className='capitalize'>
+        {label}
+      </label>
+      <input
+        className={cn(
+          'rounded-lg border-2 border-gray-400 px-2 py-1',
+          price && 'pl-7'
+        )}
+        id={label}
+        type={type}
+        autoComplete='off'
+        defaultValue={defaultValue}
+        {...register(label, inputConfig)}
+      />
+      {price && <span className='absolute top-8.5 left-2'>Rp</span>}
+    </div>
+  )
+}
+
+export default FormFieldInput
